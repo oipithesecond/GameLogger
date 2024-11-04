b6fcc886a217abc0485e6039b7432c0a306464b9
oipi <tuberosumgames@gmail.com>
2024-11-04 16:13:05 +0530

    react

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Profile(){
     const[hasPlayed, setPlayed] = useState(false)
     const[hoursPlayed, setHours] = useState(0)
 
+    useEffect(() => {
+        document.title = `Hours: ${hoursPlayed}`
+        return() => {
+            document.title = "GameLogger" 
+        }
+    }, [hoursPlayed])
+
     const togglePlayedStatus = () => {
         setPlayed(!hasPlayed)
     }
     const updateHours = () => {
-        setHours(hoursPlayed + 1)
+        setHours(h => h+1)
     }
     
     return(
